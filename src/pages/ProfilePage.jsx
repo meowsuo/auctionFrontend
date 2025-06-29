ddfec76fbe15d014a1b384ada8cf1489d3ed6665
kostasmpous <kostasmpous@gmail.com>
2025-06-29 16:00:26 +0300

    added messages

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import api from "../services/api";
 
 export default function ProfilePage() {
